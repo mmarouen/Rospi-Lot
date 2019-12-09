a5fee7cc868dbc552ff9f975800d3db7189c497c
azzouz marouen <azzouz.marouen@gmail.com>
2019-12-09 06:47:05 +0100

    running loop

--- control/pid_controller/src/pid_controller.cpp
+++ control/pid_controller/src/pid_controller.cpp
@@ -62,26 +62,29 @@ int main (int argc, char **argv)
 
 	ros::NodeHandle n;
 
-	ros::Subscriber lanes_sub = n.subscribe("/control/pid/cmd_vel", 1,yaw_callback);	
+	ros::Subscriber lanes_sub = n.subscribe("/planning/trajectory/cmd_vel", 1,yaw_callback);	
     ROS_INFO("> Pid subscriber correctly initialized");
 	ros::Publisher pub_vel = n.advertise<i2cpwm_board::ServoArray>("servos_absolute",1);
     ROS_INFO("> Pid publisher correctly initialized");
     
     ros::Rate loop_rate(1);
-    int duration =20;
+    int duration =30;
     time_t current=time(NULL);
     while (time(NULL)-current<duration)
     {
 
-        if(twist.angular.z != NULL){
+        if(true){
             pidControl(twist.angular.z);
 
             i2cpwm_board::Servo throttle;
             throttle.servo =1;
-            throttle.value = twist.linear.x*0.5*range+center_val;
+            throttle.value = twist.linear.x;
             i2cpwm_board::Servo steering;
             steering.servo =2;
-            steering.value = current_alpha*0.5*range+center_val;
+            float servo2 = current_alpha*0.5*range+center_val;
+            if(servo2>378) servo2=378;
+            if(servo2<288) servo2=288;
+            steering.value = servo2;
             i2cpwm_board::ServoArray servoArray;
             servoArray.servos.insert(servoArray.servos.end(),throttle);
             servoArray.servos.insert(servoArray.servos.end(),steering);
